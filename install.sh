2513b9e8601d232cae7e83a87e19b0e7cac496c6
q1365528996 <40208515+q1365528996@users.noreply.github.com>
2022-07-23 10:55:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -805,7 +805,7 @@ install_v2ray() {
 
 	v2ray_tmp_file="/tmp/v2ray/v2ray.zip"
 	v2ray_ver="$(curl -s https://ghproxy.com/https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-	v2ray_download_link="https://ghproxy.com/https://github.com/v2ray/v2ray-core/releases/download/$v2ray_ver/v2ray-linux-${v2ray_bit}.zip"
+	v2ray_download_link="https://ghproxy.com/https://github.com/v2fly/v2ray-core/releases/download/v4.31.0/v2ray-linux-64.zip"
 
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
